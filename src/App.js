b91bc63f268697092b96644e7191532483c9a061
freyrdanielsson <freyrdanielsson@gmail.com>
2020-04-13 19:01:10 +0200

    fetch current user repos

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+import { getMyRepos } from './api';
+
 import './App.scss';
 
 function App() {
+
+  const [loading, setLoading] = useState(false);
+  const [items, setItems] = useState([]);
+  const [error, setError] = useState('');
+
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const repos = await getMyRepos();
+        setItems(repos);
+      } catch (e) {
+        setError(e.message);
+      }
+      setLoading(false);
+    };
+
+    fetchData();
+  }, [])
+
+  console.log(loading, error, items);
+  
+
   return (
     <div className="App">
+
     </div>
   );
 }
---
+++ src/api.js
@@ -0,0 +1,60 @@
+
+const baseurl = process.env.REACT_APP_GITHUB_API_URL;
+
+async function get(path) {
+    return request('GET', path);
+}
+
+async function request(method, path, data) {
+    const url = new URL(path, baseurl);
+
+    const options = {
+        method,
+        headers: {},
+    };
+
+    if (data) {
+        options.headers = { 'content-type': 'application/json' };
+        options.body = JSON.stringify(data);
+    }
+
+    // Hardcoded user for now.
+    const key = process.env.REACT_APP_GITHUB_API_KEY;
+    const token = btoa(`freyrdanielsson:${key}`);
+    options.headers['Authorization'] = `Basic ${token}`;
+
+    const response = await fetch(url.href, options);
+
+    const json = await response.json();
+
+    const { status, ok } = response;
+
+    return {
+        data: json,
+        ok,
+        status,
+    }
+}
+
+async function getMyRepos() {
+    let result;
+
+    try {
+        result = await get('/user/repos?sort=pushed');
+    } catch (e) {
+        console.error('Error fetching current user repository', e);
+        throw new Error(e)
+    }
+
+    // Incase of error unrelate to e.g connection
+    if (result && !result.ok) {
+        const { data: { error = 'Error fetching current user repository' } } = result;
+        throw new Error(error);
+    }
+
+    return result.data;
+}
+
+export {
+    getMyRepos,
+}
