091a2962903bb977b6fe16b813336c52100783ac
freyrdanielsson <freyrdanielsson@gmail.com>
2020-04-14 15:04:40 +0200

    add event cards

--- src/components/RepoEventCard/RepoEventCard.js
+++ src/components/RepoEventCard/RepoEventCard.js
@@ -4,18 +4,40 @@ import './RepoEventCard.scss';
 
 function RepoEventCard(props) {
 
-    const { event } = props;
-    console.log(event);
-    
+    const { id, type, actor, payload, created_at } = props.event;
 
+    console.log(props.event);
+    const time = created_at.slice(0, -1).split('T');
 
     return (
-        <div className="event">
+        <div id={id} className="event">
             <div className='event__header'>
-
+                <p>{type}</p>
+                <p>By: {actor.login}</p>
             </div>
             <div className='event__details'>
-                
+                <p>{`${payload.ref_type || ''} ${payload.ref || ''}`}</p>
+                {payload.commits &&
+                    <ul className='event__commit-list'>
+                        {payload.commits.map((commit, i) => {
+                            return (
+                                <li key={i}>
+                                    {commit.message}
+                                </li>
+                            );
+                        })}
+                    </ul>}
+
+                {payload.pull_request &&
+                    <div>
+                        <p>{payload.pull_request.merged ? 'Merged -' : ''} {payload.action}</p>
+                        <p>Head: {payload.pull_request.head.ref}</p>
+                        <p>Base: {payload.pull_request.base.ref}</p>
+                    </div>}
+            </div>
+            <div className='event__footer'>
+                <p>Created at:</p>
+                <span>{`${time[1]} ${time[0]}`}</span>
             </div>
         </div>
     );
